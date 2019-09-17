6783032a43fa84ec0ea26752b8c07c8ef5cfda02
Antzhub <45887993+captainsesman@users.noreply.github.com>
2019-09-17 01:01:46 +0100

    Implementation of authorization using Policy

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -35,14 +35,18 @@
                             <div class="d-flex align-items-center">
                                  <h3 class="mt-0"> <a href="{{$question->url}}"> {{$question->title}}</a></h3>
                                  <div class="ml-auto">
+                                    @can('update', $question)
                                      <a href="{{route('questions.edit', $question->id)}}" class="btn btn-sm btn-outline-info"> Edit</a>
+                                     @endcan
                                  </div>
+                                 @can('update', $question)
                                  <form class="q-delete" action="{{route('questions.destroy', $question->id)}}" method="post" >
                                     @csrf
                                     @method('DELETE')
                                     <button type="submit" class="btn btn-sm btn-outline-danger" onclick="return confirm('Are Your Sure You Want to Delete??')"> Delete</button>
                                      
                                  </form>
+                                 @endcan
                                  
                                 
                             </div>
