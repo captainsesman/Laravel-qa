a2ce7e511b05b5fd2ae9235a8119b923fdc2f83a
Antzhub <45887993+captainsesman@users.noreply.github.com>
2019-09-20 04:39:00 +0100

    Display of answers for Question and Submission of Answers Implemented

--- resources/views/questions/show.blade.php
+++ resources/views/questions/show.blade.php
@@ -61,60 +61,13 @@
             </div>
         </div>
     </div>
-    <div class="row mt-4">
-        <div class="col-md-12">
-            <div class="card">                
-                <div class="card-body">
-                    <div class="card-title">
-                        <h2>{{$question->answers_count . " " . str_plural('Answers', $question->answers_count)}}</h2>
-                    </div>
-                    <hr>
-                    @foreach($question->answers as $answer)
-                    <div class="media">
-                         <div class="d-flex flex-column vote-controls">                        
-                            <a title=" This Question is Useful" class="vote-up">
-                                
-                                <li class="fas fa-caret-up fa-3x"></li>
-                            </a>
-                             <span class="votes-count">4190</span>
-                           
-                            <a title=" This Question is not Useful" class="vote-down off">
-                                <li class="fas fa-caret-down fa-3x"></li>
-                            </a>
-                            </a>
-                             <a title=" Mark this as Best Answer " class="vote-accepted mt-2 ">
-                                
-                                <li class="fas fa-check fa-2x"></li>
-                            </a>
-                                 
-                            </a>
-                           
-                        </div>
-                        <div class="media-body">
-                            {!! $answer-> body_html!!}
-                            <div class="float-right">
-                                <span class="text-muted">Answered{{$answer->created_date}}</span>
-                                <div class="media mt-2">
-                                    <a href="{{$answer->user->url}}" class="pr-2">
-                                        <img src="{{$answer->user->avatar}}">
-                                    </a>
-                                    <div class="media-body mt-1">
-                                        <a href="{{$answer->user->url}}">
-                                            {{$answer->user->name}}
-                                        </a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <hr>
+   
+   @include('answers._index', [
 
-                    @endforeach
-                    
-                </div>
-            </div>
-        </div>
-        
-    </div>
+    'answers'=> $question->answers,
+    'answersCount'=>$question->answers_count,
+   ])
+
+   @include('answers._create')
 </div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,4 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 Route::resource('questions','QuestionController')->except('show');
 Route::get('/questions/{slug}', 'QuestionController@show')->name('questions.show');
+Route::resource('questions.answers', 'AnswersController')->except('index','create', 'show');
